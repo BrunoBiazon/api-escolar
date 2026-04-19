8e7c2a833bffa5600e74da6f418a7f6fd80d9ba4
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-19 18:14:51 -0300

    feat: record DadosCadastroProfessor, DadosEndereco | enum para especialidades do professor

---
+++ src/main/java/com/brunobiazon/api_escola/endereco/DadosEndereco.java
@@ -0,0 +1,4 @@
+package com.brunobiazon.api_escola.endereco;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String complemento,String numero) {
+}
---
+++ src/main/java/com/brunobiazon/api_escola/professor/Especialidade.java
@@ -0,0 +1,14 @@
+package com.brunobiazon.api_escola.professor;
+
+public enum Especialidade {
+
+    PORTUGUES,
+    MATEMATICA,
+    FISICA,
+    GEOGRAFIA,
+    QUIMICA,
+    HISTORIA,
+    FILOSOFIA,
+    BIOLOGIA;
+
+}
