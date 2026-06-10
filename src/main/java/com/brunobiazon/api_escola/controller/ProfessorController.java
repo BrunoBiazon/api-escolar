1386aa8ed96a0ee779ce5900e6c6cd2c63402cf9
BrunoBiazon <brunobiazon10@gmail.com>
2026-06-10 15:48:56 -0300

    feat: Migration V1 table professores utilizando flyway ( adicionado as dependencias );

--- src/main/java/com/brunobiazon/api_escola/controller/ProfessorController.java
+++ src/main/java/com/brunobiazon/api_escola/controller/ProfessorController.java
@@ -4,6 +4,7 @@ import com.brunobiazon.api_escola.professor.DadosCadastroProfessor;
 import com.brunobiazon.api_escola.professor.Professor;
 import com.brunobiazon.api_escola.professor.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class ProfessorController {
     private ProfessorRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroProfessor dados) {
         repository.save(new Professor(dados));
     }
