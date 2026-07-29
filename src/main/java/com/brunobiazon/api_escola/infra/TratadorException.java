aaee686af5f7d2059698baff5d46392a53d546e8
BrunoBiazon <brunobiazon10@gmail.com>
2026-07-29 15:32:01 -0300

    feat: Tratamento erro 400, com personalização do retorno json( somente field e mensagem )

--- src/main/java/com/brunobiazon/api_escola/infra/TratadorException.java
+++ src/main/java/com/brunobiazon/api_escola/infra/TratadorException.java
@@ -4,12 +4,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import org.springframework.validation.FieldError;
 
 @RestControllerAdvice
 public class TratadorException {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity TratarErro404() {
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
